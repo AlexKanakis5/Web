3c9d08c2dd110a0c50bc6b2ca10f90cc37719af3
AlexKanakis5 <alexiskanakis@gmail.com>
2021-01-07 17:56:08 +0200

    Try

---
+++ Vue/VueTest/router-test/src/components/Navbar.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="nav">
+    <router-link :to="{ name: 'Home'}">Home</router-link> |
+    <router-link :to="{ name: 'About'}">About</router-link>
+    <router-link :to="{ name: 'User'}">User</router-link> 
+  </div>
+  <router-view/>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+#nav a.router-link-exact-active
+{
+    color: red;
+}
+#nav 
+{
+    padding: 30px;
+    font-size: 1.34rem; 
+
+}
+#nav a 
+{
+    font-size: 1.34rem;
+    text-align: center;
+    font-weight: bold;
+    padding: 10px;
+    text-decoration: none;
+    border-radius: 4px;
+
+}
+
+</style>
---
+++ Vue/VueTest/router-test/src/views/About.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae non voluptatibus dicta repellat dolores omnis ab labore quidem ea nulla, aliquid veniam magni ex iste nisi dolore quam aut adipisci ducimus, pariatur deleniti beatae. Totam quisquam quia, reprehenderit reiciendis temporibus optio atque, laborum ipsam laboriosam doloremque dignissimos eligendi adipisci perferendis.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, facere.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, facere.</p>
+
+  </div>
+</template>
